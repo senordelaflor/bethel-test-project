c0d820f3a56265f28c993f0792ea8674ed2398e1
Pablo Martinez <senordelaflor@gmail.com>
2017-04-03 21:58:09 -0500

    Added about images. Added variables file and images-path variable

--- js/partials/mobileMenu.js
+++ js/partials/mobileMenu.js
@@ -1,15 +1,15 @@
 var mobileMenu = function() {
     var nav = document.querySelector('nav'),
         hamburger = document.getElementById('menu__hamburger'),
-        menu = document.querySelector('ul');
+        menu = document.querySelector('ul'),
 
-    var toggleMenu = function(e) {
+    toggleMenu = function(e) {
         e.preventDefault();
         bethel.animate(menu, 'menu__animation');
         nav.classList.toggle('menu__hamburger--opened');
-    };
+    },
 
-    var init = function() {
+    init = function() {
         hamburger.addEventListener('click', toggleMenu);
     };
 
